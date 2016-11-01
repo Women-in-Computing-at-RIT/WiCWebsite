00b31222a714403d788c968ad804ec82d76b12fc
Claire <noblelady@users.noreply.github.com>
2016-10-31 21:40:09 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<title>WiC at RIT</title>
+<?php include 'includes/headinfo.php';
+?>
+</head>
+<body class="WiColor">
+<?php include 'includes/wicheader.php';
+?>
+	<!--Wic Content -->
+	<div class="container main-container">
+      		<div class="row">
+        		<div class="col-md-3 WiCcolorBK meetings pull-left">
+        	        <h3>Fall Meeting Times</h3>
+					<hr>
+					<ul class="h4 nobullets">
+						<li>General Meeting: Wed &bull; 6:30pm &bull; GOL-2455</li>
+						<li>Events Committee: Mon &bull; 7:00pm &bull; GOL-2500</li>
+						<li>Outreach Committee: Fri &bull; 2:00pm &bull; GOL-2600</li>
+						<li>Public Relations Committee: Mon &bull; 6:30pm &bull; GOL-2600</li>
+            <li>WiC Allies: Mon &bull; 1:00pm &bull; GOL-2600</li>
+            <li>WiCHacks Planning Committee: Wed &bull; 1:00pm &bull; GOL-2600</li>
+						<li>Projects Committee: Thu &bull; 3:30pm &bull; GOL-2600</li>
+              <ul class="nobullets pull-right">
+                <li>Raspberry Pi: Tues &bull; 12:30pm &bull; GOL-2600</li>
+                <li>Nearables: Tues &bull; 4pm &bull; GOL-2600</li>
+                <li>Alumni App: Thurs &bull; 4pm &bull; GOL-2600</li>
+              </ul>
+					</ul>
+
+        		</div>
+        		<div class="col-md-6">
+          		<div id="wiccarousel" class="carousel slide" data-ride="carousel">
+            		<ol class="carousel-indicators">
+              			<li data-target="#wiccarousel" data-slide-to="0" class="active"></li>
+              			<li data-target="#wiccarousel" data-slide-to="1"></li>
+              			<li data-target="#wiccarousel" data-slide-to="2" ></li>
+              			<li data-target="#wiccarousel" data-slide-to="3"></li>
+              			<li data-target="#wiccarousel" data-slide-to="4"></li>
+              			<li data-target="#wiccarousel" data-slide-to="5"></li>
+            		</ol>
+            		<div class="carousel-inner" role="listbox">
+              			<div class="item active">
+                			<img class="carousel-img" src="/images/ghc2015_index2015.jpg">
+              			</div>
+                    <div class="item">
+                			<img class="carousel-img" src="/images/WH16.jpg">
+              			</div>
+              			<div class="item">
+                			<img class="carousel-img" src="/images/CH16.jpg">
+              			</div>
+              			<div class="item">
+                			<img class="carousel-img" src="/images/WiC2.jpg">
+              			</div>
+              			<div class="item">
+                			<img class="carousel-img" src="/images/WiCHacksVolunteers.jpg">
+              			</div>
+              			<div class="item">
+                			<img class="carousel-img" src="/images/vintserf_index2015.jpg">
+              			</div>
+            		</div>
+          		</div>
+      		</div>
+      			<div class="col-md-3 WiCcolorBK featNws">
+            <h3>Featured News</h3><hr>
+
+            <div class="row">
+						<div class="col-md-12">
+							<i class="fa fa-calendar fa-1x"></i>
+							<a href="https://www.youtube.com/watch?v=VrhxT86HW5Y">WiC students attend the Grace Hopper Conference.</a> <br/>
+
+						</div>
+					</div>
+            
+          <div class="row">
+						<div class="col-md-12">
+							<i class="fa fa-calendar fa-1x"></i>
+							<a href="https://www.youtube.com/watch?v=tacEIv64Jtk">WiC Hacks 2016 was a huge success! Check us out on TV!</a> <br/>
+
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<i class="fa fa-calendar fa-1x"></i>
+							<a href="https://www.facebook.com/WICATRIT/photos/a.10152845186096777.1073741825.144033821776/10153849043636777/?type=1&permPage=1">Datto Dedicated a Red Chair to WiC!</a> <br/>
+
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">
+							<i class="fa fa-calendar fa-1x"></i>
+				     <a href="http://bit.ly/WiC_Workshops">Submit your ideas for more WiC Projects Workshops here!</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<i class="fa fa-calendar fa-1x"></i>
+							<a href="https://drive.google.com/file/d/0BwJ3QhYbueKObmdTcVYtcEJSTzA/view?usp=sharing">WiC '14-'15 Recap Video</a> <br/>
+
+				    </div>
+				</div>
+			</div>
+
+
+	<!--<div class="announce row WiCcolorBK col-md-6">
+		<h3>Announcements</h3><hr>
+		<p>Are you an accepted student in the B. Thomas Golisano College of Computing and Information Sciences
+			for the Fall of 2016? Go <a href="http://wic-after-hours.rit.edu">here</a> to sign up
+			for WiC After Hours, our accepted student overnight program!</p>
+
+
+	</div>-->
+
+
+	</div><!-- End Container -->
+
+
+	<?php include 'includes/footer.php';?>
+
+
+</body>
+</html>
